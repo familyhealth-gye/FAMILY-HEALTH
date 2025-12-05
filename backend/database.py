07cedb365d630f8195d84a2f065b6e70f1aff0d0
familyhealth-gye <centrodeespecialidadesfamilyhe@gmail.com>
2025-12-05 20:25:07 +0000

    Auto-generated changes

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME", "familyhealth")
+# 🚨 FORZAMOS a usar SIEMPRE la URL del .env
+MONGO_URL = os.environ["MONGO_URL"]
+DB_NAME = os.environ.get("DB_NAME", "familyhealth")
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
