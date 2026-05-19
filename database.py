9e792fd5d973cf691dadefd6627683533b401ea2
familyhealth-gye <centrodeespecialidadesfamilyhe@gmail.com>
2026-05-19 09:21:48 -0500

    Delete database.py

--- database.py
+++
@@ -1,4 +0,0 @@
-"""Compatibilidad: use `from db import db, client`."""
-from db import client, db, close_client, ensure_indexes, DB_NAME, MONGO_URL
-
-__all__ = ["client", "db", "close_client", "ensure_indexes", "DB_NAME", "MONGO_URL"]
